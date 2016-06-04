3955ce37f76c77b34fb0bd82101417acb60ad47e
Juanita Soranno <juanitasoranno@gmail.com>
2016-06-04 17:21:52 -0400

    Initial commit

---
+++ CartoDB.js
@@ -0,0 +1,11 @@
+https://juanitasoranno.cartodb.com/api/v2/viz/4990e37e-2a7a-11e6-82f8-0ef24382571b/viz.json
+<link rel="stylesheet" href="http://libs.cartocdn.com/cartodb.js/v3/3.15/themes/css/cartodb.css" />
+...
+<div id="map"></div>
+...
+<script src="http://libs.cartocdn.com/cartodb.js/v3/3.15/cartodb.js"></script>
+<script>
+window.onload = function() {
+  cartodb.createVis('map', 'https://juanitasoranno.cartodb.com/api/v2/viz/4990e37e-2a7a-11e6-82f8-0ef24382571b/viz.json');
+}
+</script>
